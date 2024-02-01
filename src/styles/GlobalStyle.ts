23948fa995d5598087cb4f5dcc8e85309b306834
Kuba Flis <jakub.flis@performance-media>
2024-02-01 09:22:30 +0100

    refactor

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -3,12 +3,12 @@ import { createGlobalStyle } from 'styled-components';
 import type { Theme } from './types';
 
 const GlobalStyle = createGlobalStyle<{ theme: Theme }>`
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-        font-family: ${({ theme }) => theme.fonts.jetBrainsFont};
-    }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: ${({ theme }) => theme.fonts.jetBrainsFont};
+  }
 
   body {
     background-color: #266DCE;
