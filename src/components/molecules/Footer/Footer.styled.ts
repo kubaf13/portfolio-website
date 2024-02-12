a3a4bfe3839eed509f585059e5b0d5c4eb95bfdd
Kuba Flis <jakub.flis@performance-media>
2024-02-12 23:56:40 +0100

    footer update - WIP

--- src/components/molecules/Footer/Footer.styled.ts
+++ src/components/molecules/Footer/Footer.styled.ts
@@ -1,6 +1,34 @@
-import styled from 'styled-components';
+import Link from 'next/link';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaFileDownload } from 'react-icons/fa';
+import styled, { css } from 'styled-components';
+
+const iconProps = css`
+  width: 30px;
+  height: 30px;
+  color: white;
+`;
+
+export const Container = styled.footer`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+
+  @media ${({ theme }) => theme.media.tablet} {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(5, 1fr);
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+    grid-template-areas:
+      'line line line line line'
+      'text text text text icons';
+  }
+`;
 
 export const FooterText = styled.p`
+  grid-area: text;
   display: block;
   margin: 10px auto;
   text-align: center;
@@ -8,8 +36,50 @@ export const FooterText = styled.p`
   font-size: 12px;
 `;
 
-export const Container = styled.footer`
-  position: absolute;
-  bottom: 0;
-  width: 100%;
+export const Div = styled.div``;
+
+export const SocialIcons = styled.div`
+  grid-area: icons;
+  //margin: 20px auto;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: baseline;
+  //width: 40vw;
+`;
+
+export const IconLink = styled(Link)`
+  svg {
+    transition: color 0.5s ease-in;
+
+    &:hover,
+    &:active {
+      color: #f0d546;
+    }
+  }
+`;
+
+export const GithubIcon = styled(FaGithub)`
+  ${iconProps}
+`;
+
+export const LinkedinIcon = styled(FaLinkedin)`
+  ${iconProps}
+`;
+
+export const FileIcon = styled(FaFileDownload)`
+  ${iconProps}
+`;
+
+export const HorizontalLine = styled.hr`
+  grid-area: line;
+  border: none;
+  height: 1px;
+  background: #fff;
+  background: repeating-linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0.36),
+    rgba(255, 255, 255, 0.36) 6px,
+    transparent 6px,
+    transparent 12px
+  );
 `;
