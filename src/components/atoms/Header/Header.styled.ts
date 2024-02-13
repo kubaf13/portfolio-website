b44264798b12c9540cb9b4c2ee3934c6758b8d09
Kuba Flis <jakub.flis@performance-media>
2024-02-13 07:43:10 +0100

    fix Header styles

--- src/components/atoms/Header/Header.styled.ts
+++ src/components/atoms/Header/Header.styled.ts
@@ -4,9 +4,14 @@ export const Wrapper = styled.div`
   position: absolute;
   width: 100%;
   display: flex;
-  padding: 10px;
 
   img {
-    margin: 0 auto;
+    margin: 10px auto;
+    height: 50px;
+
+    @media ${({ theme }) => theme.media.tablet} {
+      margin-left: 10px;
+      height: 60px;
+    }
   }
 `;
